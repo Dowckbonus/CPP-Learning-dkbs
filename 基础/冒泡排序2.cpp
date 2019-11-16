664f76dc70d9341ec6762e6f0c48d2367f927a2e
Dowckbonus <45911671+Dowckbonus@users.noreply.github.com>
2019-11-16 16:41:41 +0800

    Create 冒泡排序2.cpp

---
+++ 基础/冒泡排序2.cpp
@@ -0,0 +1,33 @@
+#include <iostream> 
+using namespace std;
+
+void bubbleSort(int * arr, int len) {  // 参数1：数组首地址； 参数2：数组长度；
+	for (int i = 0; i < len - 1; i++) {
+		for (int j = 0; j < len - i - 1; j++) {  // 如果 j > j + 1 交换数字
+			if (arr[j] > arr[j + 1]) {
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+
+		}
+	}
+}
+
+void printArray(int * arr, int len) {
+	for (int i = 0; i < len; i++) {
+		cout << arr[i] << " ";
+	}
+}
+
+
+int main() {
+
+	int arr[10] = {4, 3, 9, 6, 1, 2, 10, 8, 7, 5};  // 随机数组
+
+	int len = sizeof(arr) / sizeof(arr[0]);  // 数组长度
+
+	bubbleSort(arr, len);
+
+	printArray(arr, len);
+}
